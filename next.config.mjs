472040b217e783b63cd2070d25288254997b95da
Laykay Fals <talktoleke@gmail.com>
2026-03-08 13:23:13 -0400

    Move app to local dir outside iCloud

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import os from 'os';
-import path from 'path';
-
-// Build output goes outside iCloud Drive so iCloud can't evict the files
-const distDir = path.join(os.homedir(), '.next-builds', 'mixtape-prompt-generator');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = { distDir };
+const nextConfig = {};
 export default nextConfig;
